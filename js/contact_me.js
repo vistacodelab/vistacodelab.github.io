9d5e7accdbb9849fec8a30323119d93b5b29749e
Rinoy R K <rinoy2011@gmail.com>
2023-04-14 15:31:37 +0530

    contact slack update

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,40 +17,63 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            $.ajax({
-                url: "https://newlearnpath.com/vistacodelab/contact.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+            // $.ajax({
+            //     url: "https://newlearnpath.com/vistacodelab/contact.php",
+            //     type: "POST",
+            //     data: {
+            //         name: name,
+            //         phone: phone,
+            //         email: email,
+            //         message: message
+            //     },
+            //     cache: false,
+            //     success: function() {
+            //         // Success message
+            //         $('#success').html("<div class='alert alert-success'>");
+            //         $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            //             .append("</button>");
+            //         $('#success > .alert-success')
+            //             .append("<strong>Your message has been sent. </strong>");
+            //         $('#success > .alert-success')
+            //             .append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
+            //         //clear all fields
+            //         $('#contactForm').trigger("reset");
+            //     },
+            //     error: function() {
+            //         // Fail message
+            //         $('#success').html("<div class='alert alert-danger'>");
+            //         $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            //             .append("</button>");
+            //         $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+            //         $('#success > .alert-danger').append('</div>');
+            //         //clear all fields
+            //         $('#contactForm').trigger("reset");
+            //     },
+            // });
+
+            var sdata = formatForSlack(message, 'inbox')
+            // log in console
+            console.log(sdata)
+            // post
+            $.ajax({
+            // url is what you get from activating the "Incoming WebHooks" slack integration
+            // if you leave, you should see an error message "No Team", status 404
+            url: 'https://hooks.slack.com/services/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',
+            type: 'POST',
+            processData: true,
+            data : sdata ,
+            success : function(data) {
+                // success will show on page
+                console.log(data)
+                // $('#result').html(data);
+            },
+            error: function(data){
+                // error will show error object
+                console.log(data)
+                // $('#result').html("error:"+JSON.stringify(data));
+            }
+            });
         },
         filter: function() {
             return $(this).is(":visible");
@@ -68,3 +91,14 @@ $(function() {
 $('#name').focus(function() {
     $('#success').html('');
 });
+
+function formatForSlack(msg, chan){
+    var payload ={
+      "channel":chan,
+      "username":'incoming-webhook',
+      "text": msg,
+      "icon_emoji":':ghost:'
+    };
+    // return json string of payload
+    return JSON.stringify(payload)
+  }
